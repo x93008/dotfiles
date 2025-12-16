0dfaa8f58f56997d4a7ccd07563c8c4a49b13204
x93008 <x93008@163.com>
2025-12-16 17:23:27 +0800

    [update] 设置nvim 背景色半透明

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -11,6 +11,28 @@ vim.g.loaded_netrwPlugin = 1
 -- 启用 24-bit 颜色 (nvim-tree 推荐，用于更好的图标显示)
 vim.opt.termguicolors = true
 
+-- 保持终端透明背景（让终端半透明效果生效）
+local function set_transparent_background()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", ctermbg = "NONE" })
+    vim.api.nvim_set_hl(0, "NonText", { bg = "NONE", ctermbg = "NONE" })
+    vim.api.nvim_set_hl(0, "LineNr", { bg = "NONE", ctermbg = "NONE" })
+    vim.api.nvim_set_hl(0, "Folded", { bg = "NONE", ctermbg = "NONE" })
+    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE", ctermbg = "NONE" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE", ctermbg = "NONE" })
+    vim.api.nvim_set_hl(0, "CursorLine", { bg = "NONE", ctermbg = "NONE" })
+    vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "NONE", ctermbg = "NONE" })
+end
+
+-- 立即设置透明背景
+set_transparent_background()
+
+-- 当 colorscheme 改变时，重新设置透明背景
+vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "*",
+    callback = set_transparent_background,
+})
+
 vim.opt.clipboard = 'unnamedplus' -- use system clipboard
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 vim.opt.mouse = 'a' -- allow the mouse to be used in nvim
