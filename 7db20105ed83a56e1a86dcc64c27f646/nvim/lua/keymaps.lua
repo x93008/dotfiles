2512ced01b26e488910791c8b8f2af41b1d4c7ae
x93008 <x93008@163.com>
2026-05-27 11:17:54 +0800

    [update] 查找插件新增搜索最多5个历史的功能，新增对选中光标直接搜索的功能

--- 7db20105ed83a56e1a86dcc64c27f646/nvim/lua/keymaps.lua
+++ 7db20105ed83a56e1a86dcc64c27f646/nvim/lua/keymaps.lua
@@ -47,10 +47,18 @@ vim.keymap.set('n', '<leader>n', ':NvimTreeToggle<CR>', opts)
 -- git-blame: 使用 leader s 开关 git blame
 vim.keymap.set('n', '<leader>s', ':GitBlameToggle<CR>', opts)
 
--- nvim-telescope: leader ff查找文件，leader fg 过滤并查找内容，leader fr 恢复上次搜索
+-- nvim-telescope: leader ff查找文件，leader fg 过滤并查找内容，leader fr 恢复上次搜索，leader fp 列出所有缓存的搜索
 vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<CR>', { desc = 'Telescope live grep' })
+vim.keymap.set('v', '<leader>fg', function()
+    local saved = vim.fn.getreg('"')
+    vim.cmd('noautocmd normal! y')
+    local text = vim.fn.getreg('"')
+    vim.fn.setreg('"', saved)
+    require('telescope.builtin').grep_string({ search = text, initial_mode = 'normal' })
+end, { desc = 'Grep selected text' })
 vim.keymap.set('n', '<leader>fr', ':Telescope resume<CR>', { desc = 'Telescope resume last search' })
+vim.keymap.set('n', '<leader>fp', ':Telescope pickers<CR>', { desc = 'Telescope list cached pickers' })
 
 -- 对内置注释的快捷键重新映射(gcc、gc、leader l都可以用来注释)
 vim.keymap.set('n', '<leader>l', 'gcc', { remap = true, desc = 'Toggle line comment' })
--- 7db20105ed83a56e1a86dcc64c27f646/nvim/lua/plugins/nvim-telescope.lua
+++ 7db20105ed83a56e1a86dcc64c27f646/nvim/lua/plugins/nvim-telescope.lua
@@ -1,5 +1,12 @@
 return {
     -- 文件和内容搜索插件
     'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+        defaults = {
+            cache_picker = {
+                num_pickers = 5,
+            },
+        },
+    },
 }
