e85a32770b1560da6c39ee699b609718c0f37ec2
x93008 <x93008@163.com>
2026-08-03 18:50:01 +0800

    [modify] 增加开发相关的环境变量

--- files/4c761f170e016836ff84498202b99827/.zsh/Linux/config.zsh
+++ files/4c761f170e016836ff84498202b99827/.zsh/Linux/config.zsh
@@ -10,8 +10,15 @@ else
   setxkbmap
 fi
 
+export CONAN2_BIN=conan2
+
 # 鸿蒙开发工具
 export DEVECO_SDK_HOME=/opt/ohos/cvte_sdk
 export OHOS_BASE_SDK_HOME=$DEVECO_SDK_HOME
-hdc() { "$OHOS_BASE_SDK_HOME/23/toolchains/hdc" "$@"; }
+export OHOS_SDK_TOOLCHAINS_DIR=$OHOS_BASE_SDK_HOME/23/toolchains
+hdc() { "$OHOS_SDK_TOOLCHAINS_DIR/hdc" "$@"; }
 path=("/opt/ohos/command-line-tools/bin" "$path[@]")
+
+export DEMO_P12_FILE=/opt/ohos/sign_utils/dongle_app.p12
+export DEMO_CA_G2_FILE=/opt/ohos/sign_utils/dongle_debug_ca_g2.cer
+export DEMO_P7B_FILE=/opt/ohos/sign_utils/dongleupdate_demo_profileDebug.p7b
