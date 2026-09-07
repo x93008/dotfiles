d9a59391ab62cbccae35b8ce43ce4905e5f82f4b
x93008 <x93008@163.com>
2026-09-07 15:40:20 +0800

    [feature] 将代理设置全部丢到proxy.zsh里面

--- files/4c761f170e016836ff84498202b99827/.zsh/config.zsh
+++ files/4c761f170e016836ff84498202b99827/.zsh/config.zsh
@@ -1,11 +1,8 @@
-PROXY_SERVER_IP=172.20.72.57
-MY_HOST_IP=172.20.72.57
-
 # 加载接收端的环境变量
 export LIBVA_DRIVER_NAME=iHD
 export LD_LIBRARY_PATH="."
 export EXCEED_HOTSPOT_PASSWORD="147258369"
-export EXCEED_AP_IP="$MY_HOST_IP"
+export EXCEED_AP_IP="172.20.72.57"
 export EXCEED_HOTSPOT_COUNTRY_CODE="CN"
 export EXCEED_HOTSPOT_SSID="CPQ-5G"
 export EXCEED_SHARE_DEBUG="TRUE"
@@ -14,15 +11,8 @@ export EXCEED_SHARE_DEV="TRUE"
 
 export ESS_TEST=1
 
-# 默认打开代理
-# 本机即 PROXY_SERVER_IP 时走 127.0.0.1，否则走远程 PROXY_SERVER_IP
-if ip -4 addr 2>/dev/null | grep -qwF "$PROXY_SERVER_IP"; then
-  PROXY_HOST="127.0.0.1"
-else
-  PROXY_HOST="$PROXY_SERVER_IP"
-fi
-export http_proxy="http://${PROXY_HOST}:10800"
-export https_proxy=$http_proxy
+# 加载代理设置
+source $HOME/.zsh/proxy.zsh
 
 # debuginfo 国内源
 export DEBUGINFOD_URLS="https://repo.archlinuxcn.org"
